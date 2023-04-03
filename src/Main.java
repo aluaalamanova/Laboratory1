c3073089e0e56ecbc8e65a6f1f685400aac6946b
ADMIN <221322@astanait.edu.kz>
2023-04-04 01:59:33 +0600

    problem 2 finding average

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr ={3, 2, 4, 1};
-
-        double aver = findAver(arr, arr.length);
-        System.out.println("Average is " + aver);
+        int[] arr ={3, 2, 4, 1};  // initialize an array
+        double aver = findAver(arr, arr.length);  // call findAver method to find the average
+        System.out.println("Average is " + aver);  // prints the average value
     }
 
     public static double findAver(int[] arr, int n){
-        if(n==0)
+        if(n==0)  // base case of the recursive function
             return 0;
         else{
-            double sum = findAver(arr, n-1)*(n-1);
-            sum+=arr[n-1];
-            return sum/n;
+            double sum = findAver(arr, n-1)*(n-1); // call findAver recursively to find the sum of array
+            sum+=arr[n-1];   // add the last element of the array to the sum
+            return sum/n;  // return the average
         }
-
     }
 }
