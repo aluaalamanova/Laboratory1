0c50528abc3804c76d7b9100c1d6c2f112068312
ADMIN <221322@astanait.edu.kz>
2023-04-05 02:02:16 +0600

    problem 5 fibonacci sequence

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 /**
-* Problem 4. You are given a number “n”, write the program using recursion for finding “n!”
-* @param n is an integer of which you need to find factorial.
- * @param factorial is factorial of the n, which is computed recursively.
- * @findFactorial this recursive function finds the factorial of n.
- * @return this function returns factorial of n.
+* Problem 5.
+ * You are given a number “n”, write the function for finding n-th elements in Fibonacci sequence using recursion.
+* @param n is an integer for which you need to find fibonacci.
+ * @param fibo is the sum of previous two fibonacci numbers.
+ * @findFibonacci this recursive function finds the n-th element of Fibonacci sequence
+ * @return this function returns Fibonacci number.
 */
 import java.util.Scanner;  // import the Scanner class
 
@@ -14,15 +15,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);  // create scanner to read user input
         System.out.print("Enter a number: ");  // ask the user to enter a number
         int n = sc.nextInt();  // input the variable n
-        System.out.println("Factorial of " + n + " is " +findFactorial(n));  // call the function and print the result
+        System.out.println("Fibonacci is " +findFibonacci(n));  // call the function and print the result
     }
 
-    public static int findFactorial(int n) {
+    public static int findFibonacci(int n) {
         if (n == 0 || n == 1)  // check if n is equal to 0 or 1 (base case of the recursive function)
-            return 1;  // if so, return 1, because 0! and 1! are both equal to 1
+            return n;  // if so, return n, because fibonacci of 0 and 1 equals to itself
         else {
-            int factorial = findFactorial(n - 1) * n;  // calculate factorial recursively, by multiplying previous one with n
-            return factorial;  // return the result
+            int fibo = findFibonacci(n - 2) + findFibonacci(n-1);  // calculate fibonacci recursively, by summing two previous fibonacci numbers
+            return fibo;  // return the result
         }
     }
 } 
