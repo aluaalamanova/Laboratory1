893e8ee7a7cc91f594cd4353e8f6f0b4da412f7a
ADMIN <221322@astanait.edu.kz>
2023-04-05 03:01:04 +0600

    problem 6 n power of a

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 /**
-* Problem 5.
- * You are given a number “n”, write the function for finding n-th elements in Fibonacci sequence using recursion.
-* @param n is an integer for which you need to find fibonacci.
- * @param fibo is the sum of previous two fibonacci numbers.
- * @findFibonacci this recursive function finds the n-th element of Fibonacci sequence
- * @return this function returns Fibonacci number.
+ Problem 6. You are given numbers “a” and “n”, write the function that returns “an”.
+ * @param a is an integer which is the base of power.
+* @param n is an integer which is degree of power.
+ * @param power is variable which is multiple of a and recursively calling function with arguments a and n-1.
+ * @calculatePower this recursive function finds the n-th power of a.
+ * @return this function returns power.
 */
 import java.util.Scanner;  // import the Scanner class
 
@@ -13,17 +13,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);  // create scanner to read user input
-        System.out.print("Enter a number: ");  // ask the user to enter a number
+        System.out.print("Enter a numbers: ");  // ask the user to enter a numbers
+        int a = sc.nextInt();  // input the variable a
         int n = sc.nextInt();  // input the variable n
-        System.out.println("Fibonacci is " +findFibonacci(n));  // call the function and print the result
+        System.out.println(calculatePower(a,n));  // call the function and print the result
     }
 
-    public static int findFibonacci(int n) {
-        if (n == 0 || n == 1)  // check if n is equal to 0 or 1 (base case of the recursive function)
-            return n;  // if so, return n, because fibonacci of 0 and 1 equals to itself
+    public static int calculatePower(int a, int n) {
+        if (n == 0 )  // check if n is equal to 0 (base case of the recursive function)
+            return 1;  // if so, return n, because zero power of any number is equal to 1
         else {
-            int fibo = findFibonacci(n - 2) + findFibonacci(n-1);  // calculate fibonacci recursively, by summing two previous fibonacci numbers
-            return fibo;  // return the result
+            int power = a * calculatePower(a, n-1); // сalculate the power by multiplying a by the result of recursively calling the function with a and n-1 as arguments.
+            return power;  // return the result
         }
     }
 } 
