99b1fcb846874b0b25ece29f8cc36f7af102db50
ADMIN <221322@astanait.edu.kz>
2023-04-06 12:31:49 +0600

    problem 2 finding average

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 /**
- *Problem 1. You are given a number “n” and an array of “n” elements write the function that returns minimum of them.
- * @param arr is an array of integers of which you have to find minimum.
+ *Problem 2. You are given a number “n” and an array of “n” elements, write the function that returns average of them.
+ * @param arr is an array of integers of which you have to find average.
  * @param n is the length of an array.
- * @param min is the minimum value of the array which is find by recursive function findMin.
- * findMin this function finds the minimum of an array recursively.
- * return this function returns min.
+ * @param aver is the average of the array which is find by recursive function findAverage.
+ * @findMin this function finds the average of an array recursively.
+ * @return this function returns average.
 */
 
 import java.util.Scanner;
@@ -13,22 +13,20 @@ public class Main {
         Scanner sc = new Scanner(System.in); // Create a new Scanner object to read input
         int n = sc.nextInt(); // Read an integer from the user to determine array size
         int[] arr = new int[n]; // Create an array of size n
-
         for (int i = 0; i < n; i++) { // Loop through array to read values from the user
             arr[i] = sc.nextInt();
         }
-
-        int min = findMin(arr, n); // Call the findMin method to find the minimum value
-
-        System.out.println("Min is " + min); // Print the minimum value to the console
+        double aver = findAver(arr, n);  // call findAver method to find the average
+        System.out.println("Average is " + aver);  // prints the average value
     }
 
-    public static int findMin(int[] arr, int n) {
-        if (n == 1) // Base case: if there is only one element in the array, return it
-            return arr[0];
-        else { // Recursive case: call findMin method with one less element
-            int min = findMin(arr, n - 1);
-            return Math.min(min, arr[n - 1]); // Compare minimum value found so far with last element
+    public static double findAver(int[] arr, int n){
+        if(n==0)  // base case of the recursive function
+            return 0;
+        else{
+            double sum = findAver(arr, n-1)*(n-1); // call findAver recursively to find the sum of array
+            sum+=arr[n-1];   // add the last element of the array to the sum
+            return sum/n;  // return the average
         }
     }
 }
