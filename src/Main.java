43f6b2b5adbfe929b05bcc367d3263db7c76303b
ADMIN <221322@astanait.edu.kz>
2023-04-06 09:42:14 +0600

    lab1 problem1

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
 /**
- Problem 6. You are given numbers “a” and “n”, write the function that returns “an”.
- * @param a is an integer which is the base of power.
-* @param n is an integer which is degree of power.
- * @param power is variable which is multiple of a and recursively calling function with arguments a and n-1.
- * @calculatePower this recursive function finds the n-th power of a.
- * @return this function returns power.
+ *Problem 1. You are given a number “n” and an array of “n” elements write the function that returns minimum of them.
+ * @param arr is an array of integers of which you have to find minimum.
+ * @param n is the length of an array.
+ * @param min is the minimum value of the array which is find by recursive function findMin.
+ * findMin this function finds the minimum of an array recursively.
+ * return this function returns min.
 */
-import java.util.Scanner;  // import the Scanner class
 
-
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);  // create scanner to read user input
-        System.out.print("Enter a numbers: ");  // ask the user to enter a numbers
-        int a = sc.nextInt();  // input the variable a
-        int n = sc.nextInt();  // input the variable n
-        System.out.println(calculatePower(a,n));  // call the function and print the result
+        Scanner sc = new Scanner(System.in); // Create a new Scanner object to read input
+        int n = sc.nextInt(); // Read an integer from the user to determine array size
+        int[] arr = new int[n]; // Create an array of size n
+
+        for (int i = 0; i < n; i++) { // Loop through array to read values from the user
+            arr[i] = sc.nextInt();
+        }
+
+        int min = findMin(arr, n); // Call the findMin method to find the minimum value
+
+        System.out.println("Min is " + min); // Print the minimum value to the console
     }
 
-    public static int calculatePower(int a, int n) {
-        if (n == 0 )  // check if n is equal to 0 (base case of the recursive function)
-            return 1;  // if so, return n, because zero power of any number is equal to 1
-        else {
-            int power = a * calculatePower(a, n-1); // сalculate the power by multiplying a by the result of recursively calling the function with a and n-1 as arguments.
-            return power;  // return the result
+    public static int findMin(int[] arr, int n) {
+        if (n == 1) // Base case: if there is only one element in the array, return it
+            return arr[0];
+        else { // Recursive case: call findMin method with one less element
+            int min = findMin(arr, n - 1);
+            return Math.min(min, arr[n - 1]); // Compare minimum value found so far with last element
         }
     }
-} 
+}
